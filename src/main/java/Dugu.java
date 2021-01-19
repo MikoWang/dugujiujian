45f9bf4394069037f01a1572201fe092b45f0fbf
atguigu <wangfan756580357@163.com>
2021-01-19 22:56:08 +0800

    Miko008修改

--- src/main/java/Dugu.java
+++ src/main/java/Dugu.java
@@ -3,5 +3,9 @@ import sun.applet.Main;
 public class Dugu {
     public static void main(String[] args) {
         System.out.println("jianfa dugujiujian");
+        System.out.println("jianfa 2222");
+        System.out.println("jianfa 1111");
+        System.out.println("jianfa 3333");
+
     }
 }
