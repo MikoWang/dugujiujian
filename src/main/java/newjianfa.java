db8602b7b761d6b174671535448a261d049a5af7
atguigu <wangfan756580357@163.com>
2021-01-20 11:00:19 +0800

    Miko008修改

--- src/main/java/newjianfa.java
+++ src/main/java/newjianfa.java
@@ -1,11 +1,6 @@
 public class newjianfa {
     public static void main(String[] args) {
         System.out.println("new jianfa");
-        System.out.println("new jianfa");
-        System.out.println("new jianfa");
-        System.out.println("new jianfa");
-        System.out.println("new jianfa");
-        System.out.println("new jianfa");
-        System.out.println("new jianfa");
+
     }
 }
